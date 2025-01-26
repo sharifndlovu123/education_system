c069296ef33a7005ff0fa216357c4851c1d633bf
Sharif Ndlovu <sharifndlovu@gmail.com>
2025-01-26 21:10:56 +0200

    mixin pages and comments

--- education/courses/models.py
+++ education/courses/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from .fields import OrderField
+
 class Subject(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
@@ -37,5 +41,56 @@ class Module(models.Model):
     )
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    # uses self numering to order the courses
+    order = OrderField(blank=True, for_fields=['course'])
+    class Meta:
+        ordering = ['order']
+    def __str__(self):
+        return f'{self.order}. {self.title}'
+
+
+
+# Content model that represents the modules’ contents and define a generic relation to associate any object with the content object.
+class Content(models.Model):
+    module = models.ForeignKey(
+        Module,
+        related_name='contents',
+        on_delete=models.CASCADE
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        # You use the model__in field lookup to filter the query to the ContentType objects with a model attribute that is 'text', 'video', 'image', or 'file'.
+        limit_choices_to={
+            'model__in':('text', 'video', 'image', 'file')
+        }
+    )
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+    class Meta:
+        ordering = ['order']
+
+
+#  an abstract base model that is then extended by models – each designed to store a particular type of data: text, image, video, and file.
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User,
+        related_name='%(class)s_related',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
     def __str__(self):
         return self.title
+class Text(ItemBase):
+    content = models.TextField()
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+class Video(ItemBase):
+    url = models.URLField()
+
