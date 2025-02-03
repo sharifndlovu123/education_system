eddf247aca9f250f0bd31598bd0cec3fe831be07
Sharif Ndlovu <sharifndlovu@gmail.com>
2025-02-03 13:15:58 +0200

    Refactor API URLs and views for subjects and courses

--- education/courses/api/views.py
+++ education/courses/api/views.py
@@ -1,21 +1,30 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from courses.api.serializers import SubjectSerializer, CourseSerializer
 from courses.models import Subject, Course
 from django.db.models import Count
 from courses.api.pagination import StandardPagination
 
 
-class SubjectListView(generics.ListAPIView):
+class SubjectsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
     pagination_class = StandardPagination
-class SubjectDetailView(generics.RetrieveAPIView):
-    # queryset = Subject.objects.all()
-    queryset = Subject.objects.annotate(total_courses=Count('courses'))
-    serializer_class = SubjectSerializer
+    
+# class SubjectListView(generics.ListAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
+#     pagination_class = StandardPagination
+# class SubjectDetailView(generics.RetrieveAPIView):
+#     # queryset = Subject.objects.all()
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
 
-
-class CourseListView(generics.ListAPIView):
-    queryset = Course.objects.all()
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
     serializer_class = CourseSerializer
     pagination_class = StandardPagination
+    
+# class CourseListView(generics.ListAPIView):
+#     queryset = Course.objects.all()
+#     serializer_class = CourseSerializer
+#     pagination_class = StandardPagination
