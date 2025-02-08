0097b3f7ac828a3b0b23b08e7c4674a7298aaee8
Sharif Ndlovu <sharifndlovu@gmail.com>
2025-02-08 16:00:58 +0200

    Add chat message model and WebSocket message persistence

--- education/chat/consumers.py
+++ education/chat/consumers.py
@@ -1,14 +1,51 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+from channels.generic.websocket import AsyncWebsocketConsumer
+from django.utils import timezone
+# persist
+from chat.models import Message
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope['user']
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = 'chat_%s' % self.id
+        # join room group
+        await self.channel_layer.group_add(
+            self.room_group_name, self.channel_name
+        )
         # accept connection
-        self.accept()
-    def disconnect(self, close_code):
-        pass
+        await self.accept()
+    async def disconnect(self, close_code):
+        # leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name, self.channel_name
+        )
+
+    # persist
+    async def persist_message(self, message):
+        # send message to WebSocket
+        await Message.objects.acreate(
+            user=self.user, course_id=self.id, content=message
+        )    
     # receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
+        # send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'user': self.user.username,
+                'datetime': now.isoformat(),
+            }
+        )
+
+        # persist message
+        await self.persist_message(message)
+        
+    # receive message from room group
+    async def chat_message(self, event):
         # send message to WebSocket
-        self.send(text_data=json.dumps({'message': message}))
+        await self.send(text_data=json.dumps(event))
--- education/education/settings.py
+++ education/education/settings.py
@@ -85,6 +85,16 @@ TEMPLATES = [
 ASGI_APPLICATION = 'education.asgi.application'
 WSGI_APPLICATION = 'education.wsgi.application'
 
+# Channel Layer
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
